789a809d7bef360bf16c87c59e8ba4ce71821eed
Isaiah Wright <icwright902@gmail.com>
2020-10-01 08:21:19 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link
+        v-if="authenticated"
+        to="/login"
+        v-on:click.native="logout()"
+        replace
+        >Logout</router-link
+      >
     </div>
-    <router-view/>
+    <router-view @authenticated="setAuthenticated"></router-view>
+    <router-view />
   </div>
 </template>
-
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      authenticated: false,
+      mockAccount: {
+        username: "nraboy",
+        password: "password",
+      },
+    };
+  },
+  mounted() {
+    if (!this.authenticated) {
+      this.$router.replace({ name: "login" });
+    }
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status;
+    },
+    logout() {
+      this.authenticated = false;
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/views/secure.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="secure">
+    <p>This is a secure area</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Secure",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
